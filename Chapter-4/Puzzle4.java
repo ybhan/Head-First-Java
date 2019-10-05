7045ee30201df46e703d243007d5585d0f78ca08
Yuanbo Han <hanyuanbo@icloud.com>
2019-10-05 04:27:37 -0700

    Merge Puzzle4 and Puzzle4b

--- Chapter-4/Puzzle4.java
+++ Chapter-4/Puzzle4.java
@@ -18,3 +18,14 @@ public class Puzzle4 {
         System.out.println("result " + result);
     }
 }
+
+class Puzzle4b {
+    int ivar;
+    public int doStuff(int factor) {
+        if (ivar > 100) {
+            return ivar * factor;
+        } else {
+            return ivar * (5 - factor);
+        }
+    }
+}
