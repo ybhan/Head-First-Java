104ec2f26c91e836e2d600fcbcab4c1ff92515b6
Yuanbo Han <hanyuanbo@icloud.com>
2019-10-06 19:08:19 -0700

    Add files via upload

---
+++ Chapter-10/Foo3.java
@@ -0,0 +1,8 @@
+// Won't compile
+public class Foo3 {
+    final int x;
+
+    public void go() {
+        System.out.println(x);
+    }
+}
---
+++ Chapter-10/Foo6.java
@@ -0,0 +1,8 @@
+// Will compile
+public class Foo6 {
+    int x = 12;
+
+    public static void go(final int x) {
+        System.out.println(x);
+    }
+}
