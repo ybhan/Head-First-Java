7a6fbd31fda627a0313611c432b8192be5ee5dd5
Yuanbo Han <hanyuanbo@icloud.com>
2019-10-23 20:16:33 -0700

    Change folders' name to Java style

---
+++ chap10/Foo.java
@@ -0,0 +1,8 @@
+// Will compile
+public class Foo {
+    static int x;
+
+    public void go() {
+        System.out.println(x);
+    }
+}
---
+++ chap10/Foo2.java
@@ -0,0 +1,8 @@
+// Won't compile
+public class Foo2 {
+    int x;
+
+    public static void go() {
+        System.out.println(x);
+    }
+}
