728cda0230c7ccf3d6cc1d4d410d1b3d3a345a33
Yuanbo Han <hanyuanbo@icloud.com>
2020-05-17 17:49:49 -0700

    Networking and threads

---
+++ chap15/MyRunnable.java
@@ -0,0 +1,27 @@
+public class MyRunnable implements Runnable {
+    public void run() {
+        go();
+    }
+
+    public void go() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        doMore();
+    }
+
+    public void doMore() {
+        System.out.println("top o' the stack");
+    }
+}
+
+class ThreadTester {
+    public static void main(String[] args) {
+        Runnable threadJob = new MyRunnable();
+        Thread myThread = new Thread(threadJob);
+        myThread.start();
+        System.out.println("back in main");
+    }
+}
---
+++ chap15/TestThreads.java
@@ -0,0 +1,57 @@
+public class TestThreads {
+    public static void main(String[] args) {
+        ThreadOne t1 = new ThreadOne();
+        ThreadTwo t2 = new ThreadTwo();
+        Thread one = new Thread(t1);
+        Thread two = new Thread(t2);
+        one.start();
+        two.start();
+    }
+}
+
+class Accum {
+    private static Accum a = new Accum();
+    private int counter = 0;
+
+    private Accum() { }
+
+    public static Accum getAccum() {
+        return a;
+    }
+
+    public int getCount() {
+        return counter;
+    }
+
+    public void updateCounter(int add) {
+        counter += add;
+    }
+}
+
+class ThreadOne implements Runnable {
+    Accum a = Accum.getAccum();
+
+    public void run() {
+        for (int x = 0; x < 98; x++) {
+            a.updateCounter(1000);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) { }
+        }
+        System.out.println("one " + a.getCount());
+    }
+}
+
+class ThreadTwo implements Runnable {
+    Accum a = Accum.getAccum();
+
+    public void run() {
+        for (int x = 0; x < 99; x++) {
+            a.updateCounter(1);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) { }
+        }
+        System.out.println("two " + a.getCount());
+    }
+}
